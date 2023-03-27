f45033dc2d9f821f53dbcb32f6b37f60884f7d7d
Octavian Okumu <octaviookumu@gmail.com>
2023-03-28 01:05:00 +0300

    Add Header open close sidebar logic

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { IoMdArrowForward } from "react-icons/io";
+import { FiTrash2 } from "react-icons/fi";
+import CartItem from "./CartItem";
+import { SidebarContext } from "../contexts/SidebarContext";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isOpen, handleClose } = useContext(SidebarContext);
+  return (
+    <div
+      className={`${
+        isOpen ? "right-0" : "-right-full"
+      } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[2rem]`}
+    >
+      <div className="flex items-center justify-between py-6 border-b">
+        <div className="uppercase text-sm font-semibold">Shopping Bag (0)</div>
+        <button className="w-8 h-8 flex items-center justify-center" onClick={handleClose}>
+          <IoMdArrowForward className="text-2xl" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const ProductContext = createContext();
 
@@ -10,7 +10,6 @@ const ProductProvider = ({ children }) => {
       const response = await fetch("https://fakestoreapi.com/products");
       const data = await response.json();
       setProducts(data);
-      console.log("data", data);
     };
     fetchProducts();
 
